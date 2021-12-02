f972781080f780023db5d7a3144221543d889f29
Thomas Epicharme <tepicharme.dev@protonmail.com>
2021-12-02 14:45:25 +0400

    Création controlleur Pays, affichage et enregistrement en dur fonctionnel

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Country;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +13,22 @@ class CountryController extends AbstractController
     /**
      * @Route("/country_add", name="country_add")
      */
-    public function index(ManagerRegistry $managerRegistry): Response
+    public function addCountry(ManagerRegistry $managerRegistry): Response
     {
+        /* À commenter pour ne pas ajouter de doublons dans la base
+        $country = new Country();
+        $country->setName("Chili");
+
+        $entity_manager = $managerRegistry->getManager();
+        $entity_manager->persist($country);
+        $entity_manager->flush();
+        */
+
+        $repository = $managerRegistry->getRepository(Country::class);
+        $countries = $repository->findAll();
+
         return $this->render('country/index.html.twig', [
-            'controller_name' => 'CountryController',
+            'countries' => $countries,
         ]);
     }
 }
