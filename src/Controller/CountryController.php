f90673c1b8f1504754896778a420bec77e14cb46
MDD97 <dylan.dev974@gmail.com>
2021-12-03 14:44:46 +0400

    Modification des affichages combattants

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -17,17 +17,17 @@ class CountryController extends AbstractController
     {
 
 //         À commenter pour ne pas ajouter de doublons dans la base
-        $countries_array = array('Mexique', 'Australie', 'Corée du Sud', 'Croatie', 'Écosse', 'Cameroun', 'Brésil', 'États-Unis', 'Iran', 'Daghestan', 'Nouvelle-Zélande', 'Irlande', 'Russie',  'Nigéria', 'Royaume-Uni', 'Italie', 'République Tchèque', 'Arabie Saoudite', 'Tchétchénie', 'Chine', 'Argentine', 'Norvège', 'France', 'Jamaïque', 'Pologne', 'Autriche', 'Suisse', 'Géorgie', 'Moldavie');
-        sort($countries_array, SORT_STRING );
-
-        foreach ($countries_array as $name) {
-            $country = new Country();
-            $country->setName($name);
-
-            $entity_manager = $managerRegistry->getManager();
-            $entity_manager->persist($country);
-            $entity_manager->flush();
-        }
+//        $countries_array = array('Mexique', 'Australie', 'Corée du Sud', 'Croatie', 'Écosse', 'Cameroun', 'Brésil', 'États-Unis', 'Iran', 'Daghestan', 'Nouvelle-Zélande', 'Irlande', 'Russie',  'Nigéria', 'Royaume-Uni', 'Italie', 'République Tchèque', 'Arabie Saoudite', 'Tchétchénie', 'Chine', 'Argentine', 'Norvège', 'France', 'Jamaïque', 'Pologne', 'Autriche', 'Suisse', 'Géorgie', 'Moldavie');
+//        sort($countries_array, SORT_STRING );
+//
+//        foreach ($countries_array as $name) {
+//            $country = new Country();
+//            $country->setName($name);
+//
+//            $entity_manager = $managerRegistry->getManager();
+//            $entity_manager->persist($country);
+//            $entity_manager->flush();
+//        }
 
         $repository = $managerRegistry->getRepository(Country::class);
         $countries = $repository->findAll();
--- src/Entity/FighterMen.php
+++ src/Entity/FighterMen.php
@@ -6,6 +6,7 @@ use App\Repository\FighterMenRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=FighterMenRepository::class)
